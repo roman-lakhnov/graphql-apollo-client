b47ed572bb684b2e6657b1c217c038aba3c48917
Roman Lakhnov <lakhnov.roman@gmail.com>
2025-05-18 10:20:35 +0300

    useQuery

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,38 @@
+import {
+	ApolloClient,
+	ApolloProvider,
+	gql,
+	InMemoryCache
+} from '@apollo/client'
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import * as ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const client = new ApolloClient({
+	uri: 'https://flyby-router-demo.herokuapp.com/',
+	cache: new InMemoryCache()	
+})
+
+client
+	.query({
+		query: gql`
+			query GetLocations {
+				locations {
+					id
+					name
+					description
+					photo
+				}
+			}
+		`
+	})
+	.then(result => console.log(result))
+
+const root = ReactDOM.createRoot(document.getElementById('root')!)
+root.render(
+	<StrictMode>
+		<ApolloProvider client={client}>
+			<App />
+		</ApolloProvider>
+	</StrictMode>
 )
